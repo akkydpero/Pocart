2f8500051fdfd5a3257f3344eacafa1d40f08364
Akky <73732252+akkydpero@users.noreply.github.com>
2022-10-29 22:04:06 +0530

    Sign-in Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Store } from './Store';
@@ -12,37 +15,69 @@ import { Badge } from 'primereact/badge';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SignInScreen';
 
+
 function App() {
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar bg="dark" variant="dark" sticky="top">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>Pocart</Navbar.Brand>
               </LinkContainer>
-              <Nav >
-                <Link to="/cart" className="nav-link" >
+              <Nav>
+                <Link to="/cart" className="nav-link">
                   <i
                     className="pi pi-shopping-cart p-text-secondary p-overlay-badge"
                     style={{
                       fontSize: '1.5rem',
                       color: 'white',
-          
                     }}
                   >
                     {cart.cartItems.length > 0 && (
                       <Badge
-                        value={cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                        value={cart.cartItems.reduce(
+                          (a, c) => a + c.quantity,
+                          0
+                        )}
                         size="small"
                         severity="danger"
                       ></Badge>
                     )}
                   </i>
                 </Link>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>My Orders</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign in
+                  </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
