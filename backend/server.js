4aa94dcaaa94de234ec79cc97502b384fec82f54
Akky <73732252+akkydpero@users.noreply.github.com>
2023-04-17 10:57:17 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.get('/api/keys/google', (req, res) => {
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 app.use((err, req, res, next) => {
